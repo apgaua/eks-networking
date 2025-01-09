a797a9a3acad6bd4bd07cfc6791419dc95e66586
apgaua <apgaua.sousa@gmail.com>
2025-01-08 21:00:45 -0300

    # Everything

--- private_subnets.tf
+++ private_subnets.tf
@@ -1,43 +1,43 @@
-resource "aws_subnet" "private" {
-  count             = length(var.private_subnets)
-  vpc_id            = module.vpc.vpc_id
-  cidr_block        = var.private_subnets[count.index].cidr
-  availability_zone = var.private_subnets[count.index].availability_zone
+# resource "aws_subnet" "private" {
+#   count             = length(var.private_subnets)
+#   vpc_id            = module.vpc.vpc_id
+#   cidr_block        = var.private_subnets[count.index].cidr
+#   availability_zone = var.private_subnets[count.index].availability_zone
 
-  tags = {
-    Name = var.private_subnets[count.index].name
-  }
+#   tags = {
+#     Name = var.private_subnets[count.index].name
+#   }
 
-  depends_on = [
-    module.vpc.aws_vpc_ipv4_cidr_block_association
-  ]
-}
+#   depends_on = [
+#     module.vpc.aws_vpc_ipv4_cidr_block_association
+#   ]
+# }
 
-resource "aws_route_table" "private" {
-  count  = length(var.private_subnets)
-  vpc_id = module.vpc.vpc_id
-  tags = {
-    Name = format("%s-%s", var.project_name, var.private_subnets[count.index].name)
-  }
-}
+# resource "aws_route_table" "private" {
+#   count  = length(var.private_subnets)
+#   vpc_id = module.vpc.vpc_id
+#   tags = {
+#     Name = format("%s-%s", var.project_name, var.private_subnets[count.index].name)
+#   }
+# }
 
-resource "aws_route" "private" {
-  count = length(var.private_subnets)
+# resource "aws_route" "private" {
+#   count = length(var.private_subnets)
 
-  route_table_id         = aws_route_table.private[count.index].id
-  destination_cidr_block = "0.0.0.0/0"
+#   route_table_id         = aws_route_table.private[count.index].id
+#   destination_cidr_block = "0.0.0.0/0"
 
-  gateway_id = aws_nat_gateway.main[
-    index(
-      var.public_subnets[*].availability_zone,
-      var.private_subnets[count.index].availability_zone
-    )
-  ].id
-}
+#   gateway_id = aws_nat_gateway.main[
+#     index(
+#       var.public_subnets[*].availability_zone,
+#       var.private_subnets[count.index].availability_zone
+#     )
+#   ].id
+# }
 
-resource "aws_route_table_association" "private" {
-  count = length(var.private_subnets)
+# resource "aws_route_table_association" "private" {
+#   count = length(var.private_subnets)
 
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
-}
+#   subnet_id      = aws_subnet.private[count.index].id
+#   route_table_id = aws_route_table.private[count.index].id
+# }
--- public_subnets.tf
+++ public_subnets.tf
@@ -1,14 +1,14 @@
-resource "aws_subnet" "public" {
-  count             = length(var.public_subnets)
-  vpc_id            = module.vpc.vpc_id
-  cidr_block        = var.public_subnets[count.index].cidr
-  availability_zone = var.public_subnets[count.index].availability_zone
+# resource "aws_subnet" "public" {
+#   count             = length(var.public_subnets)
+#   vpc_id            = module.vpc.vpc_id
+#   cidr_block        = var.public_subnets[count.index].cidr
+#   availability_zone = var.public_subnets[count.index].availability_zone
 
-  tags = {
-    Name = var.public_subnets[count.index].name
-  }
+#   tags = {
+#     Name = var.public_subnets[count.index].name
+#   }
 
-  depends_on = [
-    module.vpc.aws_vpc_ipv4_cidr_block_association
-  ]
-}
+#   depends_on = [
+#     module.vpc.aws_vpc_ipv4_cidr_block_association
+#   ]
+# }
