7c5daafd5d097e72dad5d5190525d85330a017fb
apgaua <apgaua.sousa@gmail.com>
2025-09-18 23:32:14 -0300

    Set nat_gateway_type variable

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "network" {
   default_tags         = var.default_tags
   region               = var.region
   singlenat            = var.singlenat
-  # nat_gateway_type    = var.nat_gateway_type # GATEWAY | INSTANCE
+  nat_gateway_type = var.nat_gateway_type # GATEWAY or INSTANCE | Default: INSTANCE
   privatesubnets      = var.privatesubnets
   publicsubnets       = var.publicsubnets
   podsubnets          = var.podsubnets
